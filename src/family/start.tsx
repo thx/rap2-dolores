2440da3f9f70405f29a79d6c39a8a91ad94bc1e0
bigfengyu <kuailewudi@yeah.net>
2019-10-17 10:37:37 +0800

    fix: 更改 material ui 导入方式

--- src/family/start.tsx
+++ src/family/start.tsx
@@ -4,11 +4,10 @@ import { Store } from 'redux'
 import { Provider } from 'react-redux'
 import { History } from 'history'
 import { ConnectedRouter } from 'connected-react-router'
-import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider'
+import { MuiThemeProvider } from '@material-ui/core/styles/'
 import { SnackbarProvider } from 'notistack'
 import { withStyles } from '@material-ui/core'
 import GlobalStyles from '../components/common/GlobalStyles'
-import ThemeProvider from '@material-ui/core/styles/MuiThemeProvider'
 import MuiTheme from '../components/common/MuiTheme'
 import Routes from 'routes'
 import { PropTypes } from 'family'
@@ -27,17 +26,15 @@ const start = (
     }
     render() {
       return (
-        <ThemeProvider theme={MuiTheme}>
-          <MuiThemeProvider theme={MuiTheme}>
-            <SnackbarProvider maxSnack={3}>
-              <Provider store={store}>
-                <ConnectedRouter history={history}>
-                  <Routes/>
-                </ConnectedRouter>
-              </Provider>
-            </SnackbarProvider>
-          </MuiThemeProvider>
-        </ThemeProvider>
+        <MuiThemeProvider theme={MuiTheme}>
+          <SnackbarProvider maxSnack={3}>
+            <Provider store={store}>
+              <ConnectedRouter history={history}>
+                <Routes/>
+              </ConnectedRouter>
+            </Provider>
+          </SnackbarProvider>
+        </MuiThemeProvider>
       )
     }
   }
